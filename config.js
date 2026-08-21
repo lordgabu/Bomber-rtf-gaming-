01fe2040725c0bef2fa1d21cc77b55ffda8fedc5
Gabu <vinodsharma98176@gmail.com>
2026-08-21 18:00:58 +0530

    Update BOT_TOKEN and ADMIN_IDS in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-  BOT_TOKEN: "8212356485:AAGeN3peo9uHPG8eCLFRuWjs12hCVC-jNs4",
-  ADMIN_IDS: [6346250222],
+  BOT_TOKEN: "8309836877:AAExGWQIRGC_5P-XzM9mG4fmRVB0-wf7E9I",
+  ADMIN_IDS: [8565990029],
   MONGODB_URL: "mongodb+srv://sahajada07:Sahajada123@cluster0.vynn0ht.mongodb.net/?appName=Cluster0",
   DB_NAME: "otp_bomber",
 };
